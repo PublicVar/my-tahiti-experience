e31770f3b35ebb8a931aa4205b951dd93801301c
PublicVar <dev.publicvar@gmail.com>
2017-03-17 18:32:59 +0400

    remove console.log

--- server/routes/index.js
+++ server/routes/index.js
@@ -15,17 +15,11 @@ router.get('/', function (req, res, next) {
  * Interactive action with chat bot
  */
 const actions = {
-  send(request, response) {
-    console.log('send')
-   
+  send(request, response) { 
     return Promise.resolve();
   },
   // You should implement your custom actions here
   // See https://wit.ai/docs/quickstart
-  getLocation(request){
-
-    console.log('getLocation')
-  }
 };
 
 /**
@@ -36,7 +30,7 @@ const wit = new Wit({
   accessToken: WIT_KEY,
   actions
 });
-let sessionId = uuidV4();
+
 
 // ------------------------------------------------------------
 // Manage Context
@@ -55,13 +49,14 @@ function setContext(sessionId, ctx) {
  * Ask question to chat bot
  */
 router.post('/', function (req, res, next) {
+  let sessionId = uuidV4();
   let text = req.body.ask;
   let sessionId = uuidV4();
 
   if (text) {
     wit.converse(sessionId,text, {})
     .then((data) => {
-      return res.status(200).json(data.msg);
+      return res.status(200).json({msg:data.msg, richMedia: data.entities.intent});
     })
     .catch((err) => {
         return res.status(500).send('Oops! Got an error from Wit: ' + err.stack || err);
